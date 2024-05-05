7ebc52ed65c66117ce3a137cde98328d1e72fbeb
liuweijs <liuweijs@haodf.com>
2024-05-05 20:09:52 +0800

    regidter

--- conf/mysql.go
+++ conf/mysql.go
@@ -13,7 +13,7 @@ var Db = map[string]DbConfig{
 	"db1": {
 		DriverName: "mysql",
 		Dsn:        "root:8d7157cd828766d5@tcp(127.0.0.1:3306)/sso?charset=utf8mb4&parseTime=true&loc=Local",
-		// Dsn:          "sso:NXkjRBH8i78YDxRL@tcp(127.0.0.1:3306)/sso?charset=utf8mb4&parseTime=true&loc=Local",
+		// Dsn:          "root:root@tcp(127.0.0.1:3306)/sso?charset=utf8mb4&parseTime=true&loc=Local",
 		ShowSql:      true,
 		ShowExecTime: false,
 		MaxIdle:      10,
