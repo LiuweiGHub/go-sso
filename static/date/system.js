5b6edc33720ba5d33af0afc5b92b1e59bb7ea653
liuweijs <liuweijs@haodf.com>
2025-02-09 19:38:17 +0800

    替换API && 提取_menu.html

--- static/date/system.js
+++ static/date/system.js
@@ -28,7 +28,7 @@ var tgdzColor = {
     '子': 'blue',
     '丑': '#995001',
 }
-url = 'https://zydx.win/@2.0/';
+url = 'https://zydx.win/';
 
 function createXmlHttp() {
     if (window.XMLHttpRequest) {
